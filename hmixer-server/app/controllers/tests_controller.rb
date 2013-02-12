a1f4ced9c286b21f1560376fa183f328bc816b25
Josh Rumbut <josh@myimedia.com>
2013-02-12 17:49:39 -0500

    completes basic rails to hmixer functionality

--- hmixer-server/app/controllers/tests_controller.rb
+++ hmixer-server/app/controllers/tests_controller.rb
@@ -2,6 +2,8 @@ class TestsController < ApplicationController
   def metrics
     #@submissions = Submission.includes(:user, :contributions).find(1) #Submission 1 is currently a default defined in seeds.rb
     @submissions = Submission.find(1, :include => [:user, :contributions => :metric])
+    @hmixer = submission_to_hmixer(@submissions)
+    @hmixer_json = @hmixer.to_json() 
     #@submissions = Submission.all(:include => [:user, :contributions])
     @submissions_json = @submissions.to_json(:include => [:user, :contributions => {:include => :metric}])
     respond_to do |format|
@@ -28,6 +30,27 @@ class TestsController < ApplicationController
 
   def create
 
+  end
+
+  private
+  def submission_to_hmixer(submission)
+    @contributions_male = submission.contributions.where(:gender => "male")
+    @contributions_female = submission.contributions.where(:gender => "female")
+    
+    @metrics_male = @contributions_male.map{|c| {:name => c.metric.name, :features => 
+        {:healthyrange => [c.healthy_min,c.healthy_max], :totalrange => [c.total_min,c.total_max], 
+        :boundayflags => [false,true], :weight => c.weight, :unitlabel => c.metric.unit} } }
+
+    @metrics_female = @contributions_female.map{|c| {:name => c.metric.name, :features => 
+        {:healthyrange => [c.healthy_min,c.healthy_max], :totalrange => [c.total_min,c.total_max], 
+        :boundayflags => [false,true], :weight => c.weight, :unitlabel => c.metric.unit} } }
+    
+    male = { :gender => "male", :metrics => @metrics_male }
+    female = { :gender => "female", :metrics => @metrics_female }
+
+    hmixer = [male, female]
+
+    return hmixer
   end
 
 end
