cc93c2676d9c5e304de52bf89e78a8e06f49ad7d
Josh Rumbut <josh@myimedia.com>
2013-02-11 20:33:53 -0500

    adds seed.rb values

--- hmixer-server/app/controllers/tests_controller.rb
+++ hmixer-server/app/controllers/tests_controller.rb
@@ -1,5 +1,10 @@
 class TestsController < ApplicationController
   def metrics
     @submissions = Submission.all
+    respond_to do |format|
+      format.html
+      format.json
+    end
+
   end
 end
