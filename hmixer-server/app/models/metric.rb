fe2bb8cdfba38803ef3ed356e819319c8e4d8313
Josh Rumbut <josh@myimedia.com>
2013-02-11 19:13:45 -0500

    adds tests controller

--- hmixer-server/app/models/metric.rb
+++ hmixer-server/app/models/metric.rb
@@ -1,3 +1,4 @@
 class Metric < ActiveRecord::Base
   attr_accessible :name, :unit
+  has_many :contributions
 end
--- hmixer-server/config/routes.rb
+++ hmixer-server/config/routes.rb
@@ -1,4 +1,6 @@
 HmixerServer::Application.routes.draw do
+  get "tests/metrics"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
