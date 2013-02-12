a4a1d8fc6507e7bb030c508e534393d9c09a00cd
Josh Rumbut <josh@myimedia.com>
2013-02-12 15:04:09 -0500

    points tests/metrics.json to rails

--- hmixer-server/app/controllers/tests_controller.rb
+++ hmixer-server/app/controllers/tests_controller.rb
@@ -1,7 +1,9 @@
 class TestsController < ApplicationController
   def metrics
-    @submissions = Submission.find(1, :include => [:user, :contributions]) #Submission 1 is currently a default defined in seeds.rb
-
+    #@submissions = Submission.includes(:user, :contributions).find(1) #Submission 1 is currently a default defined in seeds.rb
+    @submissions = Submission.find(1, :include => [:user, :contributions => :metric])
+    #@submissions = Submission.all(:include => [:user, :contributions])
+    @submissions_json = @submissions.to_json(:include => [:user, :contributions => {:include => :metric}])
     respond_to do |format|
       format.html
       format.json
@@ -19,11 +21,13 @@ class TestsController < ApplicationController
   end
 
   #primarily to handle created of nested Submission object
-  def new
-    @submission = Submission.new
-    @submission.user = User.find_or_create_by_email(:email => params['submitter']['email'], :full_name => params['submitter']['full_name'])
-
+  #def new
+  #  @submission = Submission.new
+  #  @submission.user = User.find_or_create_by_email(:email => params['submitter']['email'], :full_name => params['submitter']['full_name'])
+  #end
 
   def create
+
+  end
 
 end
