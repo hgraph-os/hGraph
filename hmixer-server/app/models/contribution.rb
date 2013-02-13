2cc5ff9e705fb5e8575eb077eb8a0895fed6a69e
Josh Rumbut <josh@myimedia.com>
2013-02-13 17:46:41 -0500

    completes migration, please use rake db:schema:load instead of running migrations when establishing a new server

--- hmixer-server/app/models/contribution.rb
+++ hmixer-server/app/models/contribution.rb
@@ -1,5 +1,6 @@
 class Contribution < ActiveRecord::Base
   belongs_to :submission
   belongs_to :metric
-  attr_accessible :age, :gender, :healthy_max, :healthy_min, :total_max, :total_min, :weight
+  has_one: :demographic
+  attr_accessible :healthy_max, :healthy_min, :total_max, :total_min, :weight
 end
