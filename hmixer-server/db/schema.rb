61c587e7f2820e39aeb1a019fc8189b069a86af4
Josh Rumbut <josh@myimedia.com>
2013-02-13 17:46:41 -0500

    completes migration, please use rake db:schema:load instead of running migrations when establishing a new server

--- hmixer-server/db/schema.rb
+++ hmixer-server/db/schema.rb
@@ -11,42 +11,38 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130212003901) do
+ActiveRecord::Schema.define(:version => 20130213192911) do
+
+  create_table "age_ranges", :force => true do |t|
+    t.string   "name"
+    t.integer  "age_min"
+    t.integer  "age_max"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "contributions", :force => true do |t|
     t.integer  "submission_id"
     t.integer  "metric_id"
-    t.string   "gender"
-    t.integer  "age"
-    t.decimal  "healthy_min",   :precision => 10, :scale => 0
-    t.decimal  "healthy_max",   :precision => 10, :scale => 0
-    t.decimal  "total_min",     :precision => 10, :scale => 0
-    t.decimal  "total_max",     :precision => 10, :scale => 0
-    t.integer  "weight"
-    t.datetime "created_at",                                   :null => false
-    t.datetime "updated_at",                                   :null => false
+    t.decimal  "healthy_min",    :precision => 10, :scale => 0
+    t.decimal  "healthy_max",    :precision => 10, :scale => 0
+    t.decimal  "total_min",      :precision => 10, :scale => 0
+    t.decimal  "total_max",      :precision => 10, :scale => 0
+    t.integer  "score_weight"
+    t.datetime "created_at",                                    :null => false
+    t.datetime "updated_at",                                    :null => false
+    t.integer  "demographic_id"
   end
 
   add_index "contributions", ["metric_id"], :name => "index_contributions_on_metric_id"
   add_index "contributions", ["submission_id"], :name => "index_contributions_on_submission_id"
 
-  create_table "data_managers", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+  create_table "demographics", :force => true do |t|
+    t.string   "gender"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+    t.integer  "age_ranges_id"
   end
-
-  add_index "data_managers", ["email"], :name => "index_data_managers_on_email", :unique => true
-  add_index "data_managers", ["reset_password_token"], :name => "index_data_managers_on_reset_password_token", :unique => true
 
   create_table "metrics", :force => true do |t|
     t.string   "name"
@@ -54,19 +50,6 @@ ActiveRecord::Schema.define(:version => 20130212003901) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
-
-  create_table "rails_admin_histories", :force => true do |t|
-    t.text     "message"
-    t.string   "username"
-    t.integer  "item"
-    t.string   "table"
-    t.integer  "month",      :limit => 2
-    t.integer  "year",       :limit => 8
-    t.datetime "created_at",              :null => false
-    t.datetime "updated_at",              :null => false
-  end
-
-  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
 
   create_table "submissions", :force => true do |t|
     t.integer  "user_id"
