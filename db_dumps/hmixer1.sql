ed186fc3df97760403f069acea75475f9efb9982
Josh Rumbut <josh@myimedia.com>
2013-02-14 17:21:45 -0500

    backs up start before reinstalling devise

---
+++ db_dumps/hmixer1.sql
@@ -0,0 +1,221 @@
+-- MySQL dump 10.13  Distrib 5.1.66, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: hmixer
+-- ------------------------------------------------------
+-- Server version	5.1.66-0+squeeze1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `age_ranges`
+--
+
+DROP TABLE IF EXISTS `age_ranges`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `age_ranges` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `age_min` int(11) DEFAULT NULL,
+  `age_max` int(11) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `age_ranges`
+--
+
+LOCK TABLES `age_ranges` WRITE;
+/*!40000 ALTER TABLE `age_ranges` DISABLE KEYS */;
+/*!40000 ALTER TABLE `age_ranges` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `contributions`
+--
+
+DROP TABLE IF EXISTS `contributions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `contributions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `submission_id` int(11) DEFAULT NULL,
+  `metric_id` int(11) DEFAULT NULL,
+  `healthy_min` decimal(10,0) DEFAULT NULL,
+  `healthy_max` decimal(10,0) DEFAULT NULL,
+  `total_min` decimal(10,0) DEFAULT NULL,
+  `total_max` decimal(10,0) DEFAULT NULL,
+  `weight` int(11) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `demographic_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_contributions_on_submission_id` (`submission_id`),
+  KEY `index_contributions_on_metric_id` (`metric_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contributions`
+--
+
+LOCK TABLES `contributions` WRITE;
+/*!40000 ALTER TABLE `contributions` DISABLE KEYS */;
+INSERT INTO `contributions` VALUES (1,1,1,'0','130','0','160',5,'2013-02-12 01:35:57','2013-02-12 01:35:57',NULL),(2,1,2,'50','60','0','60',5,'2013-02-12 01:35:57','2013-02-12 01:35:57',NULL),(3,1,3,'0','150','0','600',5,'2013-02-12 01:35:57','2013-02-12 01:35:57',NULL),(4,1,4,'7','10','0','18',5,'2013-02-12 01:35:57','2013-02-12 01:35:57',NULL),(5,1,1,'0','100','0','160',5,'2013-02-12 01:35:57','2013-02-12 01:35:57',NULL),(6,1,2,'40','60','0','60',5,'2013-02-12 01:35:57','2013-02-12 01:35:57',NULL),(7,1,3,'0','150','0','600',5,'2013-02-12 01:35:57','2013-02-12 01:35:57',NULL),(8,1,4,'7','10','0','18',5,'2013-02-12 01:35:57','2013-02-12 01:35:57',NULL);
+/*!40000 ALTER TABLE `contributions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `demographics`
+--
+
+DROP TABLE IF EXISTS `demographics`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `demographics` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `gender` varchar(255) DEFAULT NULL,
+  `contribution_id` int(11) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_demographics_on_contribution_id` (`contribution_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `demographics`
+--
+
+LOCK TABLES `demographics` WRITE;
+/*!40000 ALTER TABLE `demographics` DISABLE KEYS */;
+/*!40000 ALTER TABLE `demographics` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `metrics`
+--
+
+DROP TABLE IF EXISTS `metrics`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `metrics` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `unit` varchar(255) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `metrics`
+--
+
+LOCK TABLES `metrics` WRITE;
+/*!40000 ALTER TABLE `metrics` DISABLE KEYS */;
+INSERT INTO `metrics` VALUES (1,'LDL','mg/dl','2013-02-12 01:30:13','2013-02-12 01:30:13'),(2,'HDL','mg/dl','2013-02-12 01:30:13','2013-02-12 01:30:13'),(3,'Triglycerides','mg/dl','2013-02-12 01:30:13','2013-02-12 01:30:13'),(4,'Sleep','hours/night','2013-02-12 01:30:13','2013-02-12 01:30:13'),(5,'LDL','mg/dl','2013-02-12 01:32:54','2013-02-12 01:32:54'),(6,'HDL','mg/dl','2013-02-12 01:32:54','2013-02-12 01:32:54'),(7,'Triglycerides','mg/dl','2013-02-12 01:32:54','2013-02-12 01:32:54'),(8,'Sleep','hours/night','2013-02-12 01:32:54','2013-02-12 01:32:54'),(9,'LDL','mg/dl','2013-02-12 01:35:57','2013-02-12 01:35:57'),(10,'HDL','mg/dl','2013-02-12 01:35:57','2013-02-12 01:35:57'),(11,'Triglycerides','mg/dl','2013-02-12 01:35:57','2013-02-12 01:35:57'),(12,'Sleep','hours/night','2013-02-12 01:35:57','2013-02-12 01:35:57');
+/*!40000 ALTER TABLE `metrics` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `schema_migrations`
+--
+
+DROP TABLE IF EXISTS `schema_migrations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `schema_migrations` (
+  `version` varchar(255) NOT NULL,
+  UNIQUE KEY `unique_schema_migrations` (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `schema_migrations`
+--
+
+LOCK TABLES `schema_migrations` WRITE;
+/*!40000 ALTER TABLE `schema_migrations` DISABLE KEYS */;
+INSERT INTO `schema_migrations` VALUES ('20130211214156'),('20130211214221'),('20130211214319'),('20130211215510'),('20130211223441'),('20130212003231'),('20130212003901'),('20130213190849'),('20130213191346'),('20130213192205');
+/*!40000 ALTER TABLE `schema_migrations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `submissions`
+--
+
+DROP TABLE IF EXISTS `submissions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `submissions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `message` text,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_submissions_on_user_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `submissions`
+--
+
+LOCK TABLES `submissions` WRITE;
+/*!40000 ALTER TABLE `submissions` DISABLE KEYS */;
+INSERT INTO `submissions` VALUES (1,1,'Hello world of hGraph!','2013-02-12 01:35:57','2013-02-12 01:35:57');
+/*!40000 ALTER TABLE `submissions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `full_name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Dr. Defacto','drdefacto@defactomd.com','2013-02-12 01:30:13','2013-02-12 01:30:13'),(2,'Dr. Defacto','drdefacto@defactomd.com','2013-02-12 01:32:54','2013-02-12 01:32:54'),(3,'Dr. Defacto','drdefacto@defactomd.com','2013-02-12 01:35:57','2013-02-12 01:35:57');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-02-13 17:39:54
