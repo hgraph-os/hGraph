5f7a9769f7ee860545a44e1260df8b5139a1d98b
Danny <dadley@sizethreestudios.com>
2013-06-20 11:57:08 -0400

    adding pointmanger

---
+++ src/graph/pointmanager.js
@@ -0,0 +1,34 @@
+import "component"
+
+// hGraph.Graph.PointManager
+
+// PointManagerFactory
+function PointManagerFactory( proto ) {
+    
+    proto.Update = function( ) {
+    
+        for( var i = 0; i < this.points.length; i++ )
+            this.points[i].Update( );
+        
+    };
+    
+    proto.Draw = function( ) {
+    
+        for( var i = 0; i < this.points.length; i++ )
+            this.points[i].Draw( );
+            
+    };
+    
+    proto.addPoint = function( data ) {
+        
+    };
+      
+};
+
+// PointManagerFactory (constructor)
+PointManagerFactory['constructor'] = function( ) {
+    this.points = [ ];
+};
+
+// create the constructor from the component factory
+hGraph.Graph.PointManager = hGraph.Graph.ComponentFacory( PointManagerFactory );
